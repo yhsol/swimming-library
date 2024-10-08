7a608f467390fd96bb1a085d601bbdd68ec8b884
yhsol <loshy244110@gmail.com>
2024-10-09 02:21:15 +0900

    feat: set favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Swimming Library",
   description: "Swimming Library",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
